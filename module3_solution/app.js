f8b681961d1a716ef4c3e5f9f3dc8404d502bfc5
sans <s@s.com>
2018-03-18 00:58:25 +0530

    assignment 3 solution

---
+++ module3_solution/app.js
@@ -0,0 +1,81 @@
+(function(){
+	"use strict";
+	angular.module("NarrowItDownApp",[])
+	.controller("NarrowItDownController",NarrowItDownController)
+	.service("MenuSearchService",MenuSearchService)
+	.directive("foundItems", FoundItems);
+
+	NarrowItDownController.$inject = ["MenuSearchService"];
+	function NarrowItDownController(MenuSearchService){
+		var narrow = this;
+
+		narrow.narrowlist = function(searchTerm){
+			if(searchTerm == undefined || searchTerm.trim() == ""){
+				narrow.show = true;
+				narrow.found = "";
+			}
+			else if(searchTerm != undefined && searchTerm.trim() != ""){
+				var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+				promise.then(function(result){
+					if(result.length!=0){
+						narrow.show = false;
+						narrow.found = result;	
+					}
+					else {
+						narrow.show = true;
+					}
+				}).catch(function(error){
+					console.log(error);
+				});
+			}
+			
+		};
+
+		narrow.remove = function(item){
+			narrow.found.splice(item,1);
+		};
+	}
+    
+	function FoundItems(){
+		var ddo = {
+			restrict:"E",
+			templateUrl:"foundItems.html",
+			scope:{
+				found:'<',
+				onRemove:"&"
+			},
+			controller:directiveController,
+			controllerAs:"dirctrl",
+			bindToController:true
+			
+		};
+
+		return ddo;
+	}
+
+	function directiveController(){
+		var dirctrl = this;
+	}
+
+	MenuSearchService.$inject = ["$http"];
+	function MenuSearchService($http){
+		var service = this;
+		service.getMatchedMenuItems = function(searchterm){
+			return $http({
+				url:"https://davids-restaurant.herokuapp.com/menu_items.json",
+				method:"GET"
+			}).then(function(result){
+				var founditems = [];
+				result.data.menu_items.forEach(function(ce){
+					if(ce.description.toLowerCase().indexOf(searchterm.trim().toLowerCase())>=0){
+						founditems.push(ce);
+					}
+				});
+				return founditems;
+			}).catch(function(error){
+				console.log(error);
+			});
+		};
+	}
+
+})();
